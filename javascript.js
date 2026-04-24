e0f77a31ecdad004577d930c1d45724aa9497329
Bacoocoos <jiangyuan998@gmail.com>
2026-04-23 23:22:26 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/* 
+Name: Michee Ntambwe
+File name: javascript
+Date: April/22/2026
+*/
+
+function hamburger(){
+    var menu= document.getElementById("nav-link");
+    if(menu.style.display==="block"){
+        menu.style.display="none"
+    }
+    else{
+        menu.style.display="block"
+    }
+        
+       
+}
